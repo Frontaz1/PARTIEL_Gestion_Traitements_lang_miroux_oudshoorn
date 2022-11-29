6a227d00182df7b64237346e50e9ddc69af404dc
oudshoorngaetan <gaetan.oudshorn@gmail.com>
2022-11-29 15:40:53 +0100

    contenu bdd et indication

--- partiel/src/Controller/IndicationController.php
+++ partiel/src/Controller/IndicationController.php
@@ -5,14 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Indication;
 
 class IndicationController extends AbstractController
 {
     #[Route('/indication', name: 'app_indication')]
-    public function index(): Response
+    public function indication(ManagerRegistry $doctrine): Response
     {
+        $repository=$doctrine->getRepository(Indication::class);
+        $lesIndications=$repository->findAll();
         return $this->render('indication/index.html.twig', [
             'controller_name' => 'IndicationController',
+            'indications'     => $lesIndications,
         ]);
     }
 }
