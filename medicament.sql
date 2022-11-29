34eda781d5e2895133c5a67e0719e60b2a4ccce0
olivia-lang <97451871+olivia-lang@users.noreply.github.com>
2022-11-29 15:24:00 +0100

    insert medi

---
+++ medicament.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO `medicament` (`id`, `libelle`) VALUES
+(1, 'doliprane'),
+(2, 'spasfon'),
+(3, 'ventoline'),
+(4, 'ibufène'),
+(5, 'innovair'),
+(6, 'moxylar'),
+(7, 'gascon'),
+(8, 'toplexil'),
+(9, 'paracétamol'),
+(10, 'bandage');
+
