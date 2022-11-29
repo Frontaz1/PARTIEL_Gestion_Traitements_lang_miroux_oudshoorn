5ac530214fad60847b56fc8d5a580cf36f4e4d7a
olivia-lang <olivia.la28@gmail.com>
2022-11-29 16:08:21 +0100

    affichage des medicaments et des consultations

--- partiel/src/Controller/ConsultationController.php
+++ partiel/src/Controller/ConsultationController.php
@@ -4,15 +4,34 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Consultation;
 
 class ConsultationController extends AbstractController
 {
-    #[Route('/consultation', name: 'app_consultation')]
-    public function index(): Response
+    //AFFICHAGE DES CONSULTATIONS
+    #[Route('/consultations', name: 'consultations')]
+    public function getConsultations(ManagerRegistry $doctrine): Response
     {
+        $repository = $doctrine->getRepository(Consultation::class);
+        $lesConsultations = $repository->findAll();
         return $this->render('consultation/index.html.twig', [
-            'controller_name' => 'ConsultationController',
+            'consultations' => $lesConsultations,
+            'title' => 'Liste des consultations',
+        ]);
+    }
+
+    //AFFICHAGE D'UNE CONSULTATION
+    #[Route('/consultation/{id}', name: 'consultation')]
+    public function getUneConsultation(ManagerRegistry $doctrine, $id, Request $request): Response
+    {
+        $repository = $doctrine->getRepository(Consultation::class);
+        $lesConsultations = $repository->find($id);
+        return $this->render('consultation/index.html.twig', [
+            'consultations' => $lesConsultations,
+            'title' => 'Liste des consultations',
         ]);
     }
 }
--- partiel/src/Controller/MedicamentController.php
+++ partiel/src/Controller/MedicamentController.php
@@ -4,15 +4,34 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Medicament;
 
 class MedicamentController extends AbstractController
 {
-    #[Route('/medicament', name: 'app_medicament')]
-    public function index(): Response
+    //AFFICHAGE DE TOUS LES MEDICAMENTS
+    #[Route('/medicaments', name: 'medicaments')]
+    public function getMedicaments(ManagerRegistry $doctrine): Response
     {
+        $repository = $doctrine->getRepository(Medicament::class);
+        $lesMedicaments = $repository->findAll();
         return $this->render('medicament/index.html.twig', [
-            'controller_name' => 'MedicamentController',
+            'medicaments' => $lesMedicaments,
+            'title' => 'Liste des médicaments',
         ]);
+    }
+
+    //AFFICHAGE D'UN MEDICAMENT
+    #[Route('/medicament/{id}', name: 'medicament')]
+    public function modifd(ManagerRegistry $doctrine, $id, Request $request): Response
+    {
+        $repository = $doctrine->getRepository(Medicament::class);
+        $lesMedicaments = $repository->find($id);
+        return $this->render('medicament/index.html.twig', array(
+            'medicaments' => $lesMedicaments,
+        'title' => 'Le médicament',
+    ));
     }
 }
